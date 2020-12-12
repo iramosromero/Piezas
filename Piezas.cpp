2c505bf6256c1b6ff575a26d68c000ba877e5f1e
iramosromero <ramos.israel916@gmail.com>
2020-12-11 22:33:10 -0800

    Created piezas game and tests

--- Piezas.cpp
+++ Piezas.cpp
@@ -1,5 +1,8 @@
 #include "Piezas.h"
 #include <vector>
+#include <iostream>
+
+using namespace std;
 /** CLASS Piezas
  * Class for representing a Piezas vertical board, which is roughly based
  * on the game "Connect Four" where pieces are placed in a column and 
@@ -22,6 +25,14 @@
 **/
 Piezas::Piezas()
 {
+    board.resize(3, vector<Piece> (4));
+    for (int i = 0; i < (int)board.size(); i++) {
+        for (int j = 0; j < (int)board[i].size(); j++) {
+            board[i][j] = Blank;
+        }
+    }
+    turn = X;
+
 }
 
 /**
@@ -30,6 +41,11 @@ Piezas::Piezas()
 **/
 void Piezas::reset()
 {
+    for (int i = 0; i < BOARD_ROWS; i++) {
+        for (int j = 0; j < BOARD_COLS; j++) {
+            board[i][j] = Blank;
+        }
+    }
 }
 
 /**
@@ -42,6 +58,47 @@ void Piezas::reset()
 **/ 
 Piece Piezas::dropPiece(int column)
 {
+
+    if (column > 3 || column < 0) {
+        if (turn == O) {
+            turn = X;
+        }
+        if (turn == X) {
+            turn = O;
+        }
+        return Invalid;
+    }
+
+    for(int i = 2; i >= 0; i--)
+    {
+        if(turn == X)
+        {
+            if(board[i][column] == Blank)
+            {
+                board[i][column] = turn;
+                turn = O;
+                return X;
+            }
+        }
+
+        if(turn == O)
+        {
+            if(board[i][column] == Blank)
+            {
+                board[i][column] = turn;
+                turn = X;
+                return O;
+            }
+        }
+    }
+
+    if(turn == X){
+        turn = O;
+    } else
+    {
+        turn = X;
+    }
+
     return Blank;
 }
 
@@ -51,6 +108,18 @@ Piece Piezas::dropPiece(int column)
 **/
 Piece Piezas::pieceAt(int row, int column)
 {
+    if (board[row][column] == X) {
+        return X;
+    }
+    else if (board[row][column] == O) {
+        return O;
+    }
+    else if (row > 2 || row < 0) {
+        return Invalid;
+    }
+    else if (column > 3 || column < 0) {
+        return Invalid;
+    }
     return Blank;
 }
 
@@ -65,5 +134,174 @@ Piece Piezas::pieceAt(int row, int column)
 **/
 Piece Piezas::gameState()
 {
+
+    for (int i = 2; i >= 0; i--) {
+        for (int j = 0; j < 4; j++) {
+            if (board[i][j] == Blank) {
+                return Invalid;
+            }
+        }
+    }
+
+
+    int Xrow1 = 0;
+    int Xrow2 = 0;
+    int Xrow3 = 0;
+    int Orow1 = 0;
+    int Orow2 = 0;
+    int Orow3 = 0;
+
+    int XrowSum = 0;
+    int OrowSum = 0;
+
+    for (int j = 0; j < 4; j++) {
+        if (board[2][j] == X) {
+            Xrow1++;
+        }
+        if (board[2][j] == O) {
+            Orow1++;
+        }
+        if (board[1][j] == X) {
+            Xrow2++;
+        }
+        if (board[1][j] == O) {
+            Orow2++;
+        }
+        if (board[0][j] == X) {
+            Xrow3++;
+        }
+        if (board[0][j] == O) {
+            Orow3++;
+        }
+    }
+
+    if (Xrow1 >= Xrow2 && Xrow1 >= Xrow3) {
+        XrowSum = Xrow1;
+    }
+    if (Xrow2 >= Xrow1 && Xrow2 >= Xrow3 ) {
+        XrowSum = Xrow2;
+    }
+    if (Xrow3 >= Xrow1 && Xrow3 >= Xrow2) {
+        XrowSum = Xrow3;
+    }
+    if (Orow1 >= Orow2 && Orow1 >= Orow3) {
+        OrowSum = Orow1;
+    }
+    if (Orow2 >= Orow1 && Orow2 >= Orow3) {
+        OrowSum = Orow2;
+    }
+    if (Orow3 >= Orow1 && Orow3 >= Orow2){
+        OrowSum = Orow3;
+    }
+    if ((Xrow1 == Xrow2) && (Xrow2 == Xrow3)) {
+        XrowSum = Xrow1;
+    }
+    if ((Orow1 == Orow2) && (Orow2 == Orow3)) {
+        OrowSum = Orow1;
+    }
+
+    int Xcol1 = 0;
+    int Xcol2 = 0;
+    int Xcol3 = 0;
+    int Xcol4 = 0;
+
+    int Ocol1 = 0;
+    int Ocol2 = 0;
+    int Ocol3 = 0;
+    int Ocol4 = 0;
+
+    int XcolSum = 0;
+    int OcolSum = 0;
+
+    for (int j = 0; j < 3; j++) {
+        if (board[j][0] == X) {
+            Xcol1++;
+        }
+        if (board[j][0] == O) {
+            Ocol1++;
+        }
+        if (board[j][1] == X) {
+            Xcol2++;
+        }
+        if (board[j][1] == O) {
+            Ocol2++;
+        }
+        if (board[j][2] == X) {
+            Xcol3++;
+        }
+        if (board[j][2] == O) {
+            Ocol3++;
+        }
+        if (board[j][3] == X) {
+            Xcol4++;
+        }
+        if (board[j][3] == O) {
+            Ocol4++;
+        }
+    }
+
+
+    if (Xcol3 <= Xcol1 && Xcol1 >= Xcol2) {
+        if (Xcol1 >= Xcol4) {
+            XcolSum = Xcol1;
+        }
+    }
+    if (Ocol3 <= Ocol1 && Ocol1 >= Ocol2) {
+        if (Ocol1 >= Ocol4) {
+            OcolSum = Ocol1;
+        }
+    }
+    if (Xcol3 <= Xcol2 && Xcol2 >= Xcol1) {
+        if (Xcol2 >= Xcol4) {
+            XcolSum = Xcol2;
+        }
+    }
+    if (Ocol3 <= Ocol2 && Ocol2 >= Ocol1) {
+        if (Ocol2 >= Ocol4) {
+            OcolSum = Ocol2;
+        }
+    }
+    if (Xcol1 <= Xcol3 && Xcol3 >= Xcol2) {
+        if (Xcol3 >= Xcol4) {
+            XcolSum = Xcol3;
+        }
+    }
+    if (Ocol1 <= Ocol4 && Ocol4 >= Ocol2) {
+        if (Ocol4 >= Ocol3) {
+            OcolSum = Ocol4;
+        }
+    }
+
+
+    int win_X = 0;
+    int win_O = 0;
+
+    if (XrowSum > XcolSum) {
+        win_X = XrowSum;
+    }
+    if (XcolSum > XrowSum) {
+        win_X = XcolSum;
+    }
+    if(XrowSum == XcolSum) {
+        win_X = XrowSum;
+    }
+    if (OrowSum > OcolSum) {
+        win_O = OrowSum;
+    }
+    if (OcolSum > OrowSum) {
+        win_O = OcolSum;
+    }
+    if (OrowSum == OcolSum) {
+        win_O = OrowSum;
+    }
+
+
+    if (win_X > win_O) {
+        return X;
+    }
+    if (win_O > win_X) {
+        return O;
+    }
+
     return Blank;
 }
--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -18,3 +18,203 @@ TEST(PiezasTest, sanityCheck)
 {
 	ASSERT_TRUE(true);
 }
+
+TEST(PiezasTest, isColumnFull) {
+	Piezas b;
+	b.dropPiece(0);
+	b.dropPiece(0);
+	b.dropPiece(0);
+	b.dropPiece(0);
+}
+
+TEST(PiezasTest, isFULL)
+{
+	Piezas b;
+	b.dropPiece(0);
+	b.dropPiece(1);
+	b.dropPiece(2);
+	b.dropPiece(3);
+	b.dropPiece(0);
+	b.dropPiece(1);
+	b.dropPiece(2);
+	b.dropPiece(3);
+	b.dropPiece(0);
+	b.dropPiece(1);
+	b.dropPiece(2);
+	b.dropPiece(3);
+}
+
+TEST(PiezasTest, Invalid_greater)
+{
+	Piezas b;
+	ASSERT_EQ(b.dropPiece(4), Invalid);
+}
+
+TEST(PiezasTest, Invalid_less_than)
+{
+	Piezas b;
+	ASSERT_EQ(b.dropPiece(-2), Invalid);
+}
+
+TEST(PiezasTest, check_game_empty)
+{
+	Piezas b;
+	ASSERT_EQ(b.gameState(), Invalid);
+}
+
+TEST(PiezasTest, resetboardCheck)
+{
+	Piezas b;
+	b.reset();
+}
+
+TEST(PiezasTest, DropPieceX)
+{
+	Piezas b;
+	ASSERT_EQ(X, b.dropPiece(2));
+}
+
+TEST(PiezasTest, DropPieceO)
+{
+	Piezas b;
+	b.dropPiece(0);
+	ASSERT_EQ(O, b.dropPiece(2));
+}
+
+TEST(PiezasTest, outofBounds)
+{
+	Piezas b;
+	ASSERT_EQ(Invalid, b.pieceAt(4,4));
+}
+
+TEST(PiezasTest, outofBoundsCol)
+{
+	Piezas b;
+	ASSERT_EQ(Invalid, b.pieceAt(2,4));
+}
+
+TEST(PiezasTest, outofBoundsRow)
+{
+	Piezas b;
+	ASSERT_EQ(Invalid, b.pieceAt(4,2));
+}
+
+TEST(PiezasTest, checkPieceExistX)
+{
+	Piezas b;
+	b.dropPiece(0);
+	ASSERT_EQ(X, b.pieceAt(2,0));
+}
+
+TEST(PiezasTest, checkPieceExistO)
+{
+	Piezas b;
+	b.dropPiece(0);
+	b.dropPiece(0);
+	ASSERT_EQ(O, b.pieceAt(1,0));
+}
+
+TEST(PiezasTest, checkPieceNoExist)
+{
+	Piezas b;
+	b.dropPiece(0);
+	ASSERT_FALSE(O == b.pieceAt(2,0));
+}
+
+TEST(PiezasTest, gameStateTie)
+{
+	Piezas b;
+	b.dropPiece(0);
+	b.dropPiece(1);
+	b.dropPiece(2);
+	b.dropPiece(3);
+	b.dropPiece(0);
+	b.dropPiece(1);
+	b.dropPiece(2);
+	b.dropPiece(3);
+	b.dropPiece(0);
+	b.dropPiece(1);
+	b.dropPiece(2);
+	b.dropPiece(3);
+	ASSERT_EQ(b.gameState(), Blank);
+}
+
+TEST(PiezasTest, gameStateTie2)
+{
+	Piezas b;
+	b.dropPiece(0);
+	b.dropPiece(0);
+	b.dropPiece(1);
+	b.dropPiece(1);
+	b.dropPiece(2);
+	b.dropPiece(2);
+	b.dropPiece(3);
+	b.dropPiece(3);
+	b.dropPiece(0);
+	b.dropPiece(1);
+	b.dropPiece(2);
+	b.dropPiece(3);
+	ASSERT_EQ(b.gameState(), Blank);
+}
+
+TEST(PiezasTest, gameStateTie3)
+{
+	Piezas b;
+	b.dropPiece(0);
+	b.dropPiece(1);
+	b.dropPiece(2);
+	b.dropPiece(3);
+	b.dropPiece(0);
+	b.dropPiece(1);
+	b.dropPiece(1);
+	b.dropPiece(2);
+	b.dropPiece(1);
+	b.dropPiece(1);
+	b.dropPiece(3);
+	b.dropPiece(0);
+	b.dropPiece(2);
+	b.dropPiece(3);
+	ASSERT_EQ(b.gameState(), Blank);
+
+}
+
+TEST(PiezasTest, gameState_x_wins)
+{
+	Piezas b;
+	b.dropPiece(0);
+	b.dropPiece(1);
+	b.dropPiece(2);
+	b.dropPiece(3);
+	b.dropPiece(0);
+	b.dropPiece(0);
+	b.dropPiece(1);
+	b.dropPiece(1);
+	b.dropPiece(2);
+	b.dropPiece(3);
+	b.dropPiece(2);
+	b.dropPiece(0);
+	b.dropPiece(3);
+	ASSERT_EQ(b.gameState(), X);
+}
+
+TEST(PiezasTest, gameState_o_wins)
+{
+	Piezas b;
+	b.dropPiece(4);
+	b.dropPiece(0);
+	b.dropPiece(1);
+	b.dropPiece(2);
+	b.dropPiece(3);
+	b.dropPiece(0);
+	b.dropPiece(0);
+	b.dropPiece(1);
+	b.dropPiece(1);
+	b.dropPiece(2);
+	b.dropPiece(3);
+	b.dropPiece(2);
+	b.dropPiece(0);
+	b.dropPiece(3);
+	ASSERT_EQ(b.gameState(), O);
+}
+
+
